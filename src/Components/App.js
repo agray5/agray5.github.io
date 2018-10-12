398d94e5366478a49d1d4b963010b0e08ad479ae
Unknown <grayaprild@gmail.com>
2018-10-11 23:14:45 -0500

    change header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,55 @@
+//Core
 import React, { Component } from 'react';
 import './App.css';
 
+//Depends
+import styled from 'styled-components';
+import { invert } from 'invert-color';
+
+//Components
 import SideBar from './SideBar';
 
-import styled from 'styled-components';
+//Assets 
 import wolf from '../Img/wolf.png';
+import {Colors} from '../palette'
 
 class App extends Component {
   render() {
     return (
-      <Root id="root">
+      <Root id="container">
         <SideBar />
-        <div ></div>
         <StyledImg src={wolf} />
+        <Title>April Gray</Title>
+        {/*<Divider></Divider>*/}
       </Root>
     );
   }
 }
 
+
 const Root = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(to right, ${Colors.primary} 0%,rgba(0,0,0,1) 50%);
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  transform: translate(-25%, -25%);
+  color: ${invert('#282b35') };
+`;
+
+const Divider = styled.div`
+  width: 20px;
+  height: 100%;
+  background: rgba(${Colors.secondary}, 0.9);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const StyledImg = styled.img`
---
+++ src/palette.js
@@ -0,0 +1,7 @@
+export const  Colors = {
+    primary: '#49bece',
+    secondary: '#727675',
+    accent: '#c2496a',
+    primary_dark: '#2e6171',
+    primary_light: '#556f7a'
+}
